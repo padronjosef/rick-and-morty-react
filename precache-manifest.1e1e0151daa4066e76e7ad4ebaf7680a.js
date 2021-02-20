543d6695be65ec5b91d69050e302b393b76f4079
Jose Padron <josepadron.go@gmail.com>
2021-02-20 00:11:26 -0500

    Updates

---
+++ precache-manifest.1e1e0151daa4066e76e7ad4ebaf7680a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "756db7f394917f28197b",
+    "url": "/static/css/main.415efe70.chunk.css"
+  },
+  {
+    "revision": "756db7f394917f28197b",
+    "url": "/static/js/main.27770ca3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ca1650b0275fd2012e4e",
+    "url": "/static/js/2.2d980713.chunk.js"
+  },
+  {
+    "revision": "42d6efdb5571c7fc125c19f9d1ab2b3b",
+    "url": "/static/media/logo.42d6efdb.png"
+  },
+  {
+    "revision": "2e5a132da5eef6abdbb1c6e1a0c3de6b",
+    "url": "/index.html"
+  }
+];
